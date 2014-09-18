727ef2fdc9e2de611cfb650a4857bc4b7d924311
Amirali Sanatinia <amiralis@users.noreply.github.com>
2014-09-18 14:22:07 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup
 
+import geomap
+
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
 setup(name='geomap',
-      version=0.1,
+      version=geomap.__version__,
       description='Generate a map of geolocations',
       long_description=readme,
       classifiers=[
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.2'
+          'Programming Language :: Python :: 3.2',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Topic :: Scientific/Engineering :: Visualization',
       ],
       url='https://github.com/amiralis/geo-mapper',
       author='Amirali Sanatinia',
-      email='amirali@ccs.neu.edu',
+      author_email='amirali@ccs.neu.edu',
       license='Apache 2',
       packages=['geomap'],
       scripts=['bin/geo-map'],
